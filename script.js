f2aac8d930a8c627448cbf2c6884f9cd47f27e9a
Rachit <pandeyrachit703@gmail.com>
2026-07-11 02:23:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const greetingElement = document.getElementById('greeting');
+const hour = new Date().getHours();
+let greeting;
+
+if (hour < 12) greeting = "Good Morning!";
+else if (hour < 18) greeting = "Good Afternoon!";
+else greeting = "Good Evening!";
+
+greetingElement.textContent = `${greeting} I'm Rachit.`;
